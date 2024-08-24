faf195725321f918f8ff3ea8bc970c7a12a42d52
cwzhou <cwzhou@live.unc.edu>
2024-08-24 19:25:05 -0400

    params censor

--- Simulations/Paper1_CR/CR00.Simulation_Parameters.R
+++ Simulations/Paper1_CR/CR00.Simulation_Parameters.R
@@ -1,16 +1,19 @@
 # things to note:
 # - censoring for training; no censoring for testing; truncated at tau for both (differs by sim generate failure setting)
 # right now, in cr01 t0_pmcr is set to 0.2 regardless of other parameters.
-# local = 0 #0 # local = 0 for cluster
-#### libraries and functions
+# local = 0 # local = 0 for cluster
+
+# below is only needed if running this script directly. comment out if running CR01.Simulation_Run.R script.
 # if (local == 1){
 #   setwd("~/Desktop/UNC_BIOS_PhD/DissertationPhD/Thesis/Code/Analyses/Simulations/Paper1_CR")
 # } else{
 #   setwd("/nas/longleaf/home/cwzhou/Dissertation/Analyses/Simulations/Paper1_CR")
 # }
+
+#### libraries and functions
 source("F01.Simulation_Functions.R") # calls libraries
 
-date_folder = Sys.Date() #"2024-02-27" # this is the most recent date with results; # very old date: "2024-02-18"
+date_folder = "2024-08-20" #Sys.Date() #"2024-02-27" # this is the most recent date with results; # very old date: "2024-02-18"
 # date_folder = Sys.Date()
 n.eval = 1000 #n.eval = 10000
 n.sim = 200
