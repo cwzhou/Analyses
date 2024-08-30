9824ac93f531c8729ab08b904f5a6a7cda65adb2
cwzhou <cwzhou@live.unc.edu>
2024-08-30 17:57:11 -0400

    change to cluster not local

--- Simulations/Paper1_CR/CR00.Simulation_Parameters.R
+++ Simulations/Paper1_CR/CR00.Simulation_Parameters.R
@@ -1,15 +1,15 @@
 # things to note:
 # - censoring for training; no censoring for testing; truncated at tau for both (differs by sim generate failure setting)
 # right now, in cr01 t0_pmcr is set to 0.2 regardless of other parameters.
-local = 1 # local = 0 for cluster
-
-# below is only needed if running this script directly. comment out if running CR01.Simulation_Run.R script.
-# uncomment if running alone (aka for CR02.Simulation_Summary.R
-if (local == 1){
-   setwd("~/Desktop/UNC_BIOS_PhD/DissertationPhD/Thesis/Code/Analyses/Simulations/Paper1_CR")
- } else{
-   setwd("/nas/longleaf/home/cwzhou/Dissertation/Analyses/Simulations/Paper1_CR")
- }
+# local = 1 # local = 0 for cluster
+#
+# # below is only needed if running this script directly. comment out if running CR01.Simulation_Run.R script.
+# # uncomment if running alone (aka for CR02.Simulation_Summary.R
+# if (local == 1){
+#    setwd("~/Desktop/UNC_BIOS_PhD/DissertationPhD/Thesis/Code/Analyses/Simulations/Paper1_CR")
+#  } else{
+#    setwd("/nas/longleaf/home/cwzhou/Dissertation/Analyses/Simulations/Paper1_CR")
+#  }
 
 #### libraries and functions
 source("F01.Simulation_Functions.R") # calls libraries
