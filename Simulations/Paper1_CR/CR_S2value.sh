6636365552149200f31705acaef61eb64982f2ea
cwzhou <cwzhou@live.unc.edu>
2024-08-30 19:33:14 -0400

    rever cr

--- Simulations/Paper1_CR/CR_S2value.sh
+++ Simulations/Paper1_CR/CR_S2value.sh
@@ -1,10 +1,10 @@
-for l in 1 #{1..2} # cause 1 probabilty for CR # change to just 1 for simple_exp
+for l in {1..2} # cause 1 probabilty for CR # change to just 1 for simple_exp
   do for k in 1 #{1..2}  #crit(both surv and endpoint = stick together), 1-2
-    do for j in 1 #{1..2}  #size = 1,2
-      do for i in 1 #{1..2} #propensity = 1,2
-        do for h in 1 #{1..2} #{1..4} beta = 1,2,3,4
+    do for j in {1..2}  #size = 1,2
+      do for i in {1..2} #propensity = 1,2
+        do for h in {1..2} #{1..4} beta = 1,2,3,4
           #ncauses = 2
-            do for g in 1 #{1..2} # censor is either low (1) or high (2)
+            do for g in {1..2} # censor is either low (1) or high (2)
    	      #endpoint = CR
                 do sbatch -t 07-05:00:00  --mail-type=fail --mail-user=cwzhou@email.unc.edu --mem=9000 CR_S0run.sh CR01.Simulation_Run.R 1 $g 1 $h $i $j $k $k $l $1  #$1 = date
       	      #done
