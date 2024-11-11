b3d57a0a9842897b832792e12b501ae7eec0ee18
cwzhou <cwzhou@live.unc.edu>
2024-11-11 16:51:07 -0500

    Remove Simulations/scratch from tracking

--- Simulations/scratch/Scratch/reading_RDS_testing.R
+++
@@ -1,2 +0,0 @@
-tmp = readRDS("CR/output/simResult_2023-12-08_nCauses1_cause1prob1_beta1_prop2_n1_critS1_critE1.rds")
-View(tmp)
--- Simulations/scratch/Scratch/sim_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run script1.sh
-./CR_S2value_script1.sh
-
-# Run script2.sh after script1.sh has completed
-./script2.sh
