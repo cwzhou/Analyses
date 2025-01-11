5c2bbe5402895cd70f3d42f64fb772089320954b
cwzhou <cwzhou@live.unc.edu>
2025-01-10 23:44:33 -0500

    ntree to 300

--- Simulations/Paper3_RE/02.Simulation_Parameters_RE.R
+++ Simulations/Paper3_RE/02.Simulation_Parameters_RE.R
@@ -22,7 +22,7 @@ n.sim = 500
 sim_data_type = "RE"
 endpoint = sim_data_type
 tau0 = 10
-ntree1 = 100
+ntree1 = 300
 ##### Gap Time Hyperparameters #####
 G = 10 #5 # total gap times
 # now defined in F01.DynamicsRE.R
