2510b175e0e4e2a3c7843059e1d1c2b660a8ea38
cwzhou <cwzhou@live.unc.edu>
2025-07-19 15:12:51 -0400

    update lib for cluster

--- Simulations/Paper1_CR/F00.Simulation_Libraries.R
+++ Simulations/Paper1_CR/F00.Simulation_Libraries.R
@@ -10,7 +10,24 @@ library(survival)
 library(dplyr)
 # library(randomForestSRC)
 library(knitr)
-library(itrSurv)
+if (local == 1){
+  library(itrSurv)
+} else{
+  # install most recent version of method in the cluster using terminal as needed.
+  
+  # # Install itrSurv:
+  #   # In cluster:
+  #   install.packages('~/Dissertation/Paper_1/Paper1_Method/itrSurv_backup/itrSurv_0.1.0.tar.gz', repos = NULL, type='source')
+  # library(itrSurv)
+  # 
+  # devtools::install('/nas/longleaf/home/cwzhou/Dissertation/Paper_1/Paper1_Method/itrSurv_working_package/itrSurv')
+  # 
+  # R CMD INSTALL -l /nas/longleaf/home/cwzhou/Dissertation/Paper_1/Paper1_Method/itrSurv_working_package/ itrSurv_0.1.0.tar.gz
+  # ll -t /nas/longleaf/home/cwzhou/R/x86_64-pc-linux-gnu-library/4.1
+  
+  # note this is in the 4.1 library, which may be outdated
+  library(itrSurv, lib.loc = '/nas/longleaf/home/cwzhou/R/x86_64-pc-linux-gnu-library/4.1')
+}
 # library(dtrSurv) # we include this within the body scripts directly
 library(MASS)
 library(reshape2)
