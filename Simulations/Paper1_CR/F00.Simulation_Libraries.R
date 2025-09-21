993283f0c6039a83675af5f1d24b1b6f1d2af890
cwzhou <cwzhou@live.unc.edu>
2025-09-21 17:28:43 -0400

    install dependencies and packages BEFORE running

--- Simulations/Paper1_CR/F00.Simulation_Libraries.R
+++ Simulations/Paper1_CR/F00.Simulation_Libraries.R
@@ -3,6 +3,8 @@
 # Author: Christina Zhou
 # Date: 07.02.2023
 
+message("libraries must be pre-installed. REFER TO README.MDs on Github as needed!")
+
 # Libraries --------------------------------------------------------------
 library(tidyverse)
 library(tidyr)
@@ -10,24 +12,29 @@ library(survival)
 library(dplyr)
 # library(randomForestSRC)
 library(knitr)
-if (local == 1){
-  library(itrSurv)
-} else{
-  # install most recent version of method in the cluster using terminal as needed.
-  
-  # # Install itrSurv:
-  #   # In cluster:
-  #   install.packages('~/Dissertation/Paper_1/Paper1_Method/itrSurv_backup/itrSurv_0.1.0.tar.gz', repos = NULL, type='source')
-  # library(itrSurv)
-  # 
-  # devtools::install('/nas/longleaf/home/cwzhou/Dissertation/Paper_1/Paper1_Method/itrSurv_working_package/itrSurv')
-  # 
-  # R CMD INSTALL -l /nas/longleaf/home/cwzhou/Dissertation/Paper_1/Paper1_Method/itrSurv_working_package/ itrSurv_0.1.0.tar.gz
-  # ll -t /nas/longleaf/home/cwzhou/R/x86_64-pc-linux-gnu-library/4.1
-  
-  # note this is in the 4.1 library, which may be outdated
-  library(itrSurv, lib.loc = '/nas/longleaf/home/cwzhou/R/x86_64-pc-linux-gnu-library/4.1')
-}
+# if (local == 1){
+#   library(itrSurv)
+# } else{
+#   # install most recent version of method in the cluster using terminal as needed.
+#   
+#   # # Install itrSurv:
+#   #   # In cluster:
+#   #   install.packages('~/Dissertation/Paper_1/Paper1_Method/itrSurv_backup/itrSurv_0.1.0.tar.gz', repos = NULL, type='source')
+#   # library(itrSurv)
+#   # 
+#   # devtools::install('/nas/longleaf/home/cwzhou/Dissertation/Paper_1/Paper1_Method/itrSurv_working_package/itrSurv')
+#   # 
+#   # R CMD INSTALL -l /nas/longleaf/home/cwzhou/Dissertation/Paper_1/Paper1_Method/itrSurv_working_package/ itrSurv_0.1.0.tar.gz
+#   # ll -t /nas/longleaf/home/cwzhou/R/x86_64-pc-linux-gnu-library/4.1
+#   
+#   # note this is in the 4.1 library, which may be outdated
+#   # library(itrSurv, lib.loc = '/nas/longleaf/home/cwzhou/R/x86_64-pc-linux-gnu-library/4.1')
+#   # library(devtools)
+#   # devtools::install_github("cwzhou/itrSurv")
+#   library(itrSurv)
+# }
+
+library(itrSurv)
 # library(dtrSurv) # we include this within the body scripts directly
 library(MASS)
 library(reshape2)
